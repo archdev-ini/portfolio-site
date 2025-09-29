9e7e53383cfa1839945f51df4131b649a02d981f
Inioluwa Oladipupo <officialinioluwaoladipupo@gmail.com>
2025-09-29 09:08:34 +0000

    Remove admin page

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,4 +1,3 @@
 // Flows will be imported for their side effects in this file.
 import './flows/chat-flow';
-import './flows/generate-project-details-flow';
-import './flows/generate-journal-entry-flow';
+import './flows/summarize-post-flow';
